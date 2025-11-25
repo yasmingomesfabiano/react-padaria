c901049fe8e9378012aa3f10d443ce209c4da09c
Yasmin Gomes <yasmingfabian@gimal.com>
2025-11-25 14:09:57 -0300

    primeira versão

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+
+      {/* HEADER */}
+      <div className="header">
+        <img
+          className="icone"
+          src="/usuario.png"
+          alt="Usuário"
+        />
+
+        <h1>PÃO QUENTINHO</h1>
+
+        <img
+          className="icone"
+          src="/carrinho.png"
+          alt="Carrinho"
+        />
+      </div>
+
+
+      {/* PROMOÇÕES */}
+      <p className="titulo-secao">PROMOÇÕES DO DIA</p>
+
+      <div className="promocoes">
+
+        <div className="card">
+          <img src="/pao.jpg" alt="Pão fresquinho" />
+          <p>PÃO FRANCÊS <br /> R$0,65</p>
+        </div>
+
+        <div className="card">
+          <img src="/bolo.jpg" alt="Bolo fresquinho" />
+          <p>BOLO DE FUBÁ <br /> R$7,50</p>
+        </div>
+
+        <div className="card">
+          <img src="/chocolate.jpg" alt="Chocolate" />
+          <p>CHOCOLATE <br /> R$9,50</p>
+        </div>
+
+        <div className="card">
+          <img src="/ovos-mexidos.jpg" alt="ovos" />
+          <p>OVOS-MEXIDOS <br /> R$11,50</p>
+        </div>
+
+      </div>
+
+      <hr className="linha" />
+      <img className="seta" src="/seta.png" alt="Seta" />
+
+      {/* BARRA DE PESQUISA */}
+      <div className="search-container">
+        <img className="lupa" src="/lupa(2).png" alt="Buscar" />
+
+        <input
+          className="pesquisa"
+          type="text"
+          placeholder="Digite para buscar..."
+        />
+
+        <img className="lupa" src="/lupa.png" alt="Buscar" />
+      </div>
+
+      </div>
+
   );
 }
 
